866294f57308a293870cfaa98ea2026ba4de658e
otdot <73254747+otdot@users.noreply.github.com>
2021-08-20 13:56:37 +0300

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,20 +1,62 @@
 import './App.css';
 import Navbar from './components/Navbar';
+import Button from './components/Button';
 import Home from './components/Home';
 import Products from './components/Products';
 import {
   BrowserRouter as Router,
   Route,
-  Link,
-  Switch
 } from 'react-router-dom'
+import { useState } from 'react'
 
 // import { useState } from 'react'
 
+const searchInput = document.getElementById("searchInput");
 
 function App() {
   // const [showHome, setShowHome] = useState(true)
   // const [showProduct, setShowProduct] = useState(false)
+  //Search products
+  const [searchTerm, setSearchTerm] = useState('')
+  const [showLinks, setShowLinks] = useState(false)
+  const [allProducts, setDeleteProduct] = useState([
+    {
+      title: "Kevyt Maito",
+      price: 1.7,
+      id: 1,
+      BB: "7.10.21",
+    },
+    {
+      title: "Naudan Jauheliha",
+      price: 1.7,
+      id: 2,
+      BB: "7.10.21",
+    },
+    {
+      title: "Voi",
+      price: 1.7,
+      id: 3,
+      BB: "7.10.21",
+    },
+    {
+      title: "Emmental",
+      price: 1.7,
+      id: 4,
+      BB: "7.10.21",
+    },
+    {
+      title: "Karjalanpiirakka",
+      price: 1.7,
+      id: 5,
+      BB: "7.10.21",
+    },
+    {
+      title: "Reissumies",
+      price: 1.7,
+      id: 6,
+      BB: "7.10.21",
+    },
+  ]);
 
   const NavItems = [
     {
@@ -31,18 +73,30 @@ function App() {
     }
   ]
 
-  // const clickItem= (id) => {
-  //   {id === 1 && setShowHome(!showHome)}
-  //   {id === 2 && setShowProduct(!showProduct)}
-  //   console.log('clicked', id)
-  // }
+  
+
+  const onDelete = (id) => {
+    setDeleteProduct(allProducts.filter((product) => product.id !== id))
+  }
+
   return (
     <Router>
       <div className="App">
-        <Navbar items={NavItems} />
+        <div className="Navbar">
+          <div className="leftSide">
+            <Navbar items={NavItems} showLinks={showLinks} />
+            <Button text={showLinks ? 'Close' : 'Open'}
+                color={showLinks ? 'tomato' : '#0d90d6'} 
+                openLinks={() => setShowLinks(!showLinks)}/>
+          </div>
+          <div className="rightSide">
+            <input placeholder='Search' onChange={(e) => (setSearchTerm(e.target.value))} id="searchInput" className='searchStyle' style={{backgroundColor: "snow"}}></input>
+            <Button text='search'/>
+          </div>
+        </div>
         <div className='Container'>
           <Route path='/Home' component={Home}></Route>
-          <Route path='/Products' component={Products}></Route>
+          <Route path='/Products' component={() => <Products onDelete={onDelete} searchTerm={searchTerm} allProducts={allProducts}/>}></Route>
         </div>
       </div>
     </Router>
